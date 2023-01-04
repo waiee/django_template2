0bce8a6c418e9e8d940ed8e85e481c8613b658ac
waiee <waiee_z@yahoo.com>
2023-01-04 15:48:26 +0800

    entah

--- additem/views.py
+++ additem/views.py
@@ -18,10 +18,14 @@ def additemform(request):
 def additemconfirmation(request):
 
     newitem_id = request.POST['item_id']
+    newvendor_id = request.POST['vendor_id']
     newitem_name= request.POST['item_name']
     newitem_description = request.POST['item_description']
+    newitem_quantity = request.POST['item_quantity']
+    newitem_price = request.POST['item_price']
 
-    newitem = Item(item_id = newitem_id,item_name = newitem_name, item_description =newitem_description)
+
+    newitem = Item(item_id = newitem_id,vendor_id=newvendor_id,item_name = newitem_name, item_description =newitem_description, item_quantity = newitem_quantity, item_price = newitem_price)
     newitem.save()
 
     context = {
@@ -29,5 +33,8 @@ def additemconfirmation(request):
         'item_id': newitem_id,
         'item_name': newitem_name,
         'item_description': newitem_description,
+        'vendor_id': newvendor_id,
+        'item_quantity': newitem_quantity,
+        'item_price': newitem_price,
     }
     return render(request,'additem/additemconfirmation.html',context)
