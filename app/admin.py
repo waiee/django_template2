d6f666be9a046c9253581ad2b00d6ca87c4f0ffb
waiee <waiee_z@yahoo.com>
2023-01-04 12:46:50 +0800

    register database

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from app.models import Item
+from app.models import Staff, Vendor, Item, Product, Quotation, PurchaseOrder
 
 admin.site.register(Item)
+admin.site.register(Staff)
+admin.site.register(Vendor)
+admin.site.register(Product)
+admin.site.register(Quotation)
+admin.site.register(PurchaseOrder)
