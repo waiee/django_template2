26d8eb05f234798209227a6d343e24fd8b9517d2
waiee <waiee_z@yahoo.com>
2023-01-06 22:34:27 +0800

    commit

--- viewitem/views.py
+++ viewitem/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Item
+
+def choose_item(request):
+    # Get a list of all the items in the database
+    items = Item.objects.all()
+    return render(request, 'viewitem/choose_item.html', {'items': items})
+
+def view_item(request, item_id):
+    # Get the item with the specified id
+    item = Item.objects.get(id=item_id)
+    return render(request, 'viewitem/view_item.html', {'item': item})
