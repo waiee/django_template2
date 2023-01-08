34c5371e434419c8012d629502a05db67d2df275
waiee <waiee_z@yahoo.com>
2023-01-08 22:16:30 +0800

    test

--- viewitem/views.py
+++ viewitem/views.py
@@ -8,11 +8,17 @@ from app.models import Item
 
 @login_required
 def choose_item(request):
+    context = {
+    'title': 'View Item',
+    'year': datetime.now().year,
+    }
+    context['user'] = request.user
+
     # Get a list of all the items in the database
     items = Item.objects.all()
-    return render(request, 'viewitem/choose_item.html', {'items': items})
+    return render(request, 'viewitem/choose_item.html', {'items': items}, context)
 
 def view_item(request, item_id):
     # Get the item with the specified id
-    item = Item.objects.get(id=item_id)
+    item = Item.objects.get(pk=item_id)
     return render(request, 'viewitem/view_item.html', {'item': item})
