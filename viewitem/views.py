0466da1c30f5df97db845a4c87c5d46fc6748313
waiee <waiee_z@yahoo.com>
2023-01-09 11:03:33 +0800

    commit

--- viewitem/views.py
+++ viewitem/views.py
@@ -16,9 +16,9 @@ def choose_item(request):
 
     # Get a list of all the items in the database
     items = Item.objects.all()
-    return render(request, 'viewitem/choose_item.html', {'items': items}, context)
+    return render(request, 'viewitem/choose_item.html', {'item': items}, context)
 
 def view_item(request, item_id):
     # Get the item with the specified id
-    item = Item.objects.get(pk=item_id)
+    item = Item.objects.get(id=item_id)
     return render(request, 'viewitem/view_item.html', {'item': item})
