219a5dce4566d015e7a3d9dea85ba88d8d0017e6
waiee <waiee_z@yahoo.com>
2023-01-09 12:16:42 +0800

    commit

--- viewitem/views.py
+++ viewitem/views.py
@@ -5,28 +5,32 @@ from django.contrib.auth.decorators import login_required
 from datetime import datetime
 from django.shortcuts import render
 from app.models import Item
+from django.http import HttpResponse
+from django.template import loader
 
 @login_required
 def choose_item(request):
+
+    # Get a list of all the items in the database
+    item = Item.objects.all().values()
+
     context = {
     'title': 'View Item',
     'year': datetime.now().year,
+    'data': item,
     }
     context['user'] = request.user
 
-    # Get a list of all the items in the database
-    items = Item.objects.all()
-    return render(request, 'viewitem/choose_item.html', {'item': items}, context)
+    return render(request, 'viewitem/choose_item.html', context)
 
 def view_item(request):
     # Get the item with the specified id
-    context = {
 
-        'item_id': item_id,
-        'item_name': item_name,
-        'item_description': item_description,
-        'vendor_id': vendor,
-        'item_quantity': item_quantity,
-        'item_price': item_price,
+    item = Item.objects.all().values()
+
+    context = {
+    'data': item,
     }
+
+    # return HttpResponse(template.render(context, request))
     return render(request, 'viewitem/view_item.html', context)
